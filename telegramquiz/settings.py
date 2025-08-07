9379b5041522a1b190e8a1a4863b600af68fdd74
sanduu <ermuhansandugas55@gmail.com>
2025-08-07 10:44:28 +0500

    Update settings.py with Render database config

--- telegramquiz/settings.py
+++ telegramquiz/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0x9==5!u^w)19eu2)__jikzwfab7b(b4g)0q3*7n6@^)6m=x))'
+SECRET_KEY = os.environ.get("SECRET_KEY", "insecure-default-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
@@ -79,11 +80,14 @@ WSGI_APPLICATION = 'telegramquiz.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://admin:qKAyTN0EiBkd165Us7jNbkewZpwFMCLz@dpg-d2a3d2juibrs73brcsgg-a/telegramquiz',
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
+
+
 
 
 # Password validation
@@ -122,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
